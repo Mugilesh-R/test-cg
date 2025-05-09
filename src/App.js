3c29ca3a9a12f0c9ff839ddbb55f6359c3a008ad
Mugilesh-R <mugileshrammu001@gmail.com>
2025-05-09 13:15:42 +0530

    commit all stages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CaptionGenerator from './CaptionGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <CaptionGenerator />
+    </div>
+  );
+}
+
+export default App;
